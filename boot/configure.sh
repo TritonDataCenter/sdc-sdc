78f00a986bb6fbcd9aa8dea707882f564751ebb4
Trent Mick <trentm@gmail.com>
2014-06-10 15:13:48 -0700

    HEAD-2051: SDC Service Goes Into svc-error Following Reboot

--- boot/configure.sh
+++ boot/configure.sh
@@ -37,42 +37,52 @@ datacenter_name=$(json -f ${config_path} datacenter_name)
 
 # Add a '$dcname sdc key' ssh key on the 'admin' user and to ~/.ssh in *every
 # SDC core zone* (by adding for manifest on the 'sdc' *application* in SAPI).
+#
 # This will be used for ssh'ing to each sdc zone (e.g. by the 'sdc-req' tool).
 # TODO(trent): Is that last use case still true? Else move the key to the
 #              SDC *service*.
+#
+# Note: we do this lazily on every boot in case we can't currently contact
+# the UFDS master.
 key_name="$datacenter_name sdc key"
-key=$(sdc-useradm key $admin_uuid "$key_name" || true)
-if [[ -n "$key" ]]; then
-    echo "Already have '$key_name' key on admin user"
-elif [[ "$(sdc-useradm ping --master)" != "pong" ]]; then
-    echo "Skip '$key_name' setup because cannot reach UFDS master"
+keys=$(sdc-useradm keys -j $admin_uuid || true)
+if [[ -z "$keys" ]]; then
+    echo "Warning: Could not get admin's ($admin_uuid) keys from UFDS." \
+        "Skipping '$key_name' setup."
 else
-    echo "Create '$key_name' key for admin user and add to the 'sdc' SAPI app"
-    key_file=/var/tmp/sdc.id_rsa
-    rm -f $key_file $key_file.pub
-    ssh-keygen -t rsa -C "$key_name" -f "$key_file" -N ""
-    key_fingerprint=$(ssh-keygen -l -f "$key_file" | awk '{print $2}')
+    key=$(echo "$keys" | json -c "this.name==='$key_name'" -a)
+    if [[ -n "$key" ]]; then
+        echo "Already have '$key_name' key on admin user"
+    elif [[ "$(sdc-useradm ping --master)" != "pong" ]]; then
+        echo "Skip '$key_name' setup because cannot reach UFDS master"
+    else
+        echo "Create '$key_name' key for admin user, add to the 'sdc' SAPI app"
+        key_file=/var/tmp/sdc.id_rsa
+        rm -f $key_file $key_file.pub
+        ssh-keygen -t rsa -C "$key_name" -f "$key_file" -N ""
+        key_fingerprint=$(ssh-keygen -l -f "$key_file" | awk '{print $2}')
 
-    # Add the keys to the sdc service metadata, which will be used by the
-    # actual manifests that write the keys to each 'sdc' zone.
-    sdc_app_uuid=$(sdc-sapi /applications?name=sdc | json -H 0.uuid)
-    node -e "
-        var fs = require('fs');
-        var d = {
-            metadata: {
-                SDC_PRIVATE_KEY: fs.readFileSync('$key_file', 'ascii'),
-                SDC_PUBLIC_KEY: fs.readFileSync('$key_file.pub', 'ascii'),
-                SDC_KEY_ID: '$key_fingerprint'
-            }
-        };
-        console.log(JSON.stringify(d,null,2));
-        " >/var/tmp/sdc-key-update.json
-    $SAPIADM update $sdc_app_uuid -f /var/tmp/sdc-key-update.json
+        # Add the keys to the sdc service metadata, which will be used by the
+        # actual manifests that write the keys to each 'sdc' zone.
+        sdc_app_uuid=$(sdc-sapi /applications?name=sdc | json -H 0.uuid)
+        node -e "
+            var fs = require('fs');
+            var d = {
+                metadata: {
+                    SDC_PRIVATE_KEY: fs.readFileSync('$key_file', 'ascii'),
+                    SDC_PUBLIC_KEY: fs.readFileSync('$key_file.pub', 'ascii'),
+                    SDC_KEY_ID: '$key_fingerprint'
+                }
+            };
+            console.log(JSON.stringify(d,null,2));
+            " >/var/tmp/sdc-key-update.json
+        $SAPIADM update $sdc_app_uuid -f /var/tmp/sdc-key-update.json
 
-    # Add the key to the admin user.
-    sdc-useradm add-key -n "$key_name" ${admin_uuid} ${key_file}.pub
+        # Add the key to the admin user.
+        sdc-useradm add-key -n "$key_name" ${admin_uuid} ${key_file}.pub
 
-    rm $key_file $key_file.pub /var/tmp/sdc-key-update.json
+        rm $key_file $key_file.pub /var/tmp/sdc-key-update.json
+    fi
 fi
 
 
